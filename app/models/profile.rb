2488f43dac3a5912dd0062da240813fa2d3402fb
richpeck <rpeck@frontlineutilities.co.uk>
2013-09-05 11:22:47 +0100

    Heroku Fix

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,8 +5,7 @@ class Profile < ActiveRecord::Base
 	has_attached_file :avatar,
 		:default_url => "/images/missing.png",
 		:storage => :s3,
-		:bucket => 'video-conference-demo',
-		:url => :s3_eu_url
+		:bucket => 'video-conference-demo'
 
 	def avatar_url
         avatar.url(:original)
