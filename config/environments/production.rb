f416c6d13268fe7df1d332641dc10a31799e281e
richpeck <rpeck@frontlineutilities.co.uk>
2013-09-05 10:19:16 +0100

    New Heroku Paperclip Test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,14 +78,5 @@ VideoDemo::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  # Papaerclip (for Amazon)
-  	config.paperclip_defaults = {
-  	  :storage => :s3,
-	  :bucket => ENV['video-conference-demo'],
-	  :s3_protocol => 'http',
-  	  :s3_credentials => {
-  		:access_key_id => ENV['AKIAISZOHLGVLQHH3XYA'],
-  		:secret_access_key => ENV['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']
-  	  }
-  	}
+
 end
