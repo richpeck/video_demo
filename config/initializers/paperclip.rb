5b35dad89ac665b1b1d69a075fb8a83c7810baef
richpeck <rpeck@frontlineutilities.co.uk>
2013-09-05 10:41:08 +0100

    Fixed Broken Paperclip.rb

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,1 @@
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-
-Paperclip.interpolates(:s3_eu_url) { |attachment, style|
-  "#{attachment.s3_protocol}://s3-eu-west-1.amazonaws.com/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/}, "")}"
-}
+#Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
