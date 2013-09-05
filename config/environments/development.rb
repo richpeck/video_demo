b88e06a51a84e549bc6723f73b76507db88d4be9
richpeck <rpeck@frontlineutilities.co.uk>
2013-09-05 12:01:30 +0100

    New Updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,5 +27,14 @@ VideoDemo::Application.configure do
   # number of complex assets.
   config.assets.debug = true
   
-
+  # Paperclip (for Amazon)
+  	config.paperclip_defaults = {
+  	  :storage => :s3,
+	  :bucket => ENV['video-conference-demo'],
+	  :s3_host_name => 's3-eu-west-1.amazonaws.com',
+  	  :s3_credentials => {
+  		:access_key_id => ENV['AKIAISZOHLGVLQHH3XYA'],
+  		:secret_access_key => ENV['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']
+  	  }
+  	}
 end
--- config/initializers/paperclip.rb
+++
@@ -1,3 +0,0 @@
-Paperclip.interpolates(:s3_eu_url) do |att, style|
-    "#{att.s3_protocol}://s3-eu-west-1.amazonaws.com/#{att.bucket_name}/#{att.path(style)}"
-end
