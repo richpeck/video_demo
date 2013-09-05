1e1ec8b5a92dea5c9cba938b30935364e976ef5f
richpeck <rpeck@frontlineutilities.co.uk>
2013-09-05 10:38:30 +0100

    S3 For Models

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,5 @@
-#Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-#Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+
+Paperclip.interpolates(:s3_eu_url) { |attachment, style|
+  "#{attachment.s3_protocol}://s3-eu-west-1.amazonaws.com/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/}, "")}"
+}
